08b29e4624fc7b19b846056c69cf14c6c5665ed7
Magnus Hagander <magnus@hagander.net>
2018-12-17 15:11:47 +0100

    Migrate skin specific settings into repository

--- code/skin_settings.py
+++ code/skin_settings.py
@@ -17,9 +17,19 @@ ENABLE_AUTO_ACCOUNTING = True
 
 # Emails
 DEFAULT_EMAIL = "webmaster@postgresql.eu"
+SERVER_EMAIL = "webmaster@postgresql.eu"
 TREASURER_EMAIL = "treasurer@postgresql.eu"
 INVOICE_SENDER_EMAIL = "treasurer@postgresql.eu"
 MEMBERSHIP_SENDER_EMAIL = "webmaster@postgresql.eu"
+
+# Membership
+MEMBERSHIP_LENGTH = 2
+MEMBERSHIP_COST = 10
+def MEMBERSHIP_COUNTRY_VALIDATOR(country):
+    if hasattr(country, 'europecountry'):
+        return True
+    return "Membership in PostgreSQL Europe is available to people living in geographical Europe. Exceptions to this may be provided on a case by case basis by the board. Please contact the board via email if you are seeing this message in error."
+
 
 # Ugh
 EU_VAT = True
