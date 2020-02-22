a428c53c3b93963a08b7aec9f7441c204914932b
Magnus Hagander <magnus@hagander.net>
2020-02-22 12:34:31 +0100

    Migrate cmutuel parsing code to new upstream framework
    Instead of writing code for each bank output file format this framework
    allows it to just be a JSON specification file. Which can then live in
    the upstream repo itself since it's even less likely to change.
    
    In doing so, create a migration that changes the existing
    InvoicePaymentMethod to use the new framework (while copying the config
    of it), and migrate all the existing downloaded transactions into the
    new format.
    
    Intentionally leave the old table around in the database for now, in
    case something goes wrong and we need forensics :) At a later date we'll
    add a migration to drop it.

---
+++ code/pgeusite/cmutuel/migrations/0002_migrate_to_upstream.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cmutuel', '0001_initial'),
+    ]
+
+    operations = [
+        # Transfer exisitng ledger entries to the general one
+        migrations.RunSQL("""
+INSERT INTO invoices_bankstatementrow (created, uniqueid, date, amount, description, balance, other, fromfile_id, method_id)
+SELECT
+  CURRENT_TIMESTAMP,
+  NULL,
+  opdate,
+  amount,
+  description,
+  balance,
+  jsonb_build_object('valdate', valdate),
+  NULL,
+  (SELECT id FROM invoices_invoicepaymentmethod WHERE classname='pgeusite.cmutuel.util.CMutuelPayment')
+FROM cmutuel_cmutueltransaction
+"""),
+        # Update the existing payment entries
+        migrations.RunSQL("""
+UPDATE invoices_invoicepaymentmethod SET
+  classname='postgresqleu.util.payment.banktransfer.GenericManagedBankPayment',
+  config=config || '{"filetype": "cmutuel", "definition": "", "description": "Pay using a direct IBAN bank transfer in EUR. We <strong>strongly advice</strong> not using this method if making a payment from outside the Euro-zone, as amounts must be exact and all fees covered by sender."}'::jsonb
+WHERE classname='pgeusite.cmutuel.util.CMutuelPayment'
+        """)
+    ]
